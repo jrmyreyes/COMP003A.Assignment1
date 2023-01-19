98feadcd044db190fdcf5709c95a82ed8231a098
jrmyreyes <rj.reyes024@gmail.com>
2023-01-18 19:36:00 -0800

    initial commit

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -11,7 +11,21 @@ namespace COMP003A.Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine(" ");
+            Console.WriteLine("1. Last Name: Reyes");
+            Console.WriteLine("2. First Name: Ron Jeremy");
+            Console.WriteLine("3. Age: 22");
+            Console.WriteLine("4. Place of birth: Philippines");
+            Console.WriteLine("5. Nationality: Filipino");
+            Console.WriteLine("6. Current Residency: CA, USA");
+            Console.WriteLine("7. Major: Information Systems");
+            Console.WriteLine("8. Hobbies:");
+            Console.WriteLine("9.  - Gaming");
+            Console.WriteLine("10. - Watching");
+            Console.WriteLine("11. - Eating");
+            Console.WriteLine("12. - Listening to music");
+
         }
     }
 }
