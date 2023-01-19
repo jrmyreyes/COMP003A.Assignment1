cde1fae2f90ce6acc0a6aca91fabf89ac5d21264
ronjeremyreyes <ronjeremyreyes@LEM231-11>
2023-01-18 19:15:57 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,17 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+ * 
+ * Author: Ron Jeremy Reyes
+ * Course: COMP-0003A
+ * Purpose: Assignment for week 1
+ */
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
