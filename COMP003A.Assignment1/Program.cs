d4390a05fc7f24321b9d86faec96a7f4d94d3ccd
jrmyreyes <rj.reyes024@gmail.com>
2023-01-20 18:48:34 -0800

    Intial Commit

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -26,8 +26,52 @@ namespace COMP003A.Assignment1
             Console.WriteLine("11. - Eating");
             Console.WriteLine("12. - Listening to music");
             Console.WriteLine("13. Games played:");
-            Console.WriteLine("14. (In no particular order)");
-            Console.WriteLine("");
+            Console.WriteLine("14. [In no particular order]");
+            Console.WriteLine("15. - R6:Siege");
+            Console.WriteLine("16. - CSGO ");
+            Console.WriteLine("17. - Monster Hunter World");
+            Console.WriteLine("18. - Apex Legends");
+            Console.WriteLine("19. - Valorant")
+            Console.WriteLine("20. - Warframe")
+            Console.WriteLine("21. - Dota 2");
+            Console.WriteLine("22. - LoL");
+            Console.WriteLine("23. - Valheim");
+            Console.WriteLine("24. - Nier:Automata");
+            Console.WriteLine("25. - Minecraft");
+            Console.WriteLine("26. - Terraria");
+            Console.WriteLine("27. - Raft");
+            Console.WriteLine("28. - Don't starve together");
+            Console.WriteLine("29. - Stardew Valley");
+            Console.WriteLine("30. - Tekken 7");
+            Console.WriteLine("31. - PUBG");
+            Console.WriteLine("32. - Project Zomboid");
+            Console.WriteLine("33. - No Man's Sky");
+            Console.WriteLine("34. - Fallout 3");
+            Console.WriteLine("35. - Fallout New Vegas");
+            Console.WriteLine("36. - Fallout 4");
+            Console.WriteLine("37. - Skyrim");
+            Console.WriteLine("38. - Subnautica");
+            Console.WriteLine("39. - MetalGear:V");
+            Console.WriteLine("40. - Metal Gear Rising Revengeance");
+            Console.WriteLine("41. - RE:2");
+            Console.WriteLine("42. - RE:IV");
+            Console.WriteLine("43. - RE:V");
+            Console.WriteLine("44. - RE:VI");
+            Console.WriteLine("45. - It Takes Two");
+            Console.WriteLine("46. - A Way Out");
+            Console.WriteLine("47. - The Forest");
+            Console.WriteLine("48. - Dead Space 2");
+            Console.WriteLine("49. - Dead Space 3");
+            Console.WriteLine("50. - Rust");
+            Console.WriteLine("51. - Dying Light");
+            Console.WriteLine("52. - Dead Island");
+            Console.WriteLine("53. - Left 4 Dead");
+            Console.WriteLine("54. - Left 4 Dead 2");
+            Console.WriteLine("55. - GTA:V");
+            Console.WriteLine("56. - Hollow Knight");
+            Console.WriteLine("57. - Overwatch 2");
+            Console.WriteLine("58. - Resident Evil: Village");
+            Console.WriteLine("59. - Outlast");
 
         }
     }
